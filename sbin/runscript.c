1166441a7c237b5ce8ed7478a3d41eabf891e54b
Martin Schlemmer <azarah@gentoo.org>
2002-04-05 23:21:25 +0000

    move rc-help.sh as well.
    svn path=/trunk/; revision=162

--- sbin/runscript.c
+++ sbin/runscript.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
 	}
 	myargs[new]=(char *) 0;
 	if ( argc < 3 ) {
-		execv("/etc/init.d/rc-help.sh",myargs);
+		execv("/sbin/rc-help.sh",myargs);
 		exit(1);
 	}
 	if (execv("/sbin/runscript.sh",myargs) < 0) 
