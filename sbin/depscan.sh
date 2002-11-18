1c2842e7f1d0c55e32b973502769ab3fc6e4e44d
Martin Schlemmer <azarah@gentoo.org>
2002-11-18 13:07:02 +0000

    change license
    svn path=/trunk/; revision=266

--- sbin/depscan.sh
+++ sbin/depscan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Copyright 1999-2002 Gentoo Technologies, Inc.
-# Distributed under the terms of the GNU General Public License, v2
+# Distributed under the terms of the GNU General Public License v2
 # Author:  Martin Schlemmer <azarah@gentoo.org>
 # $Header$
 
--- src/filefuncs.c
+++ src/filefuncs.c
@@ -28,8 +28,9 @@
 
 /*
  * Copyright 1999-2002 Gentoo Technologies, Inc.
+ * Distributed under the terms of the GNU General Public License v2
  * Author:  Martin Schlemmer <azarah@gentoo.org>, Nov 2002
- * $Header: &
+ * $Header$
  * 
  * Extended with: do_symlink()
  *                do_unlink()
