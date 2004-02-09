4f992ff9e9d82b574e1a20473a6a287ba62b0ce1
Martin Schlemmer <azarah@gentoo.org>
2004-02-09 18:40:44 +0000

    Add SELinux modifications to /sbin/runscript.  This allows runscript to make sure it is running in the right domain.  Patch done by Chris PeBenito <pebenito@gentoo.org>.
    svn path=/trunk/; revision=464

--- src/runscript.c
+++ src/runscript.c
@@ -10,11 +10,15 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <wait.h>
+#include <dlfcn.h>
+
+static void (*selinux_run_init) (void);
 
 int main(int argc, char **argv) {
 	char *myargs[32];
 	int new = 1;
 	myargs[0] = "runscript";
+	void *lib_handle;
 	
 /*	if (argc < 3)
 		exit(1);
@@ -28,6 +32,13 @@ int main(int argc, char **argv) {
 		execv("/lib/rcscripts/sh/rc-help.sh",myargs);
 		exit(1);
 	}
+	
+	lib_handle = dlopen("/lib/rcscripts/runscript_selinux.so", RTLD_LAZY | RTLD_GLOBAL);
+	if( lib_handle != NULL ) {
+		selinux_run_init = dlsym(lib_handle, "selinux_runscript");
+		selinux_run_init();
+	}
+
 	if (execv("/sbin/runscript.sh",myargs) < 0)
 		exit(1);
 
