2c9562082665d0ddee93b0d519ab10aa756bf58b
Aron Griffis <agriffis@gentoo.org>
2004-06-26 21:57:51 +0000

    use root:root instead of root.root
    svn path=/trunk/; revision=517

--- tarball.sh
+++ tarball.sh
@@ -9,7 +9,7 @@ do
 	cp -ax $x ${DEST}
 done
 cp ChangeLog ${DEST}
-chown -R root.root ${DEST}
+chown -R root:root ${DEST}
 chmod 0755 ${DEST}/sbin/*
 chmod 0755 ${DEST}/init.d/*
 ( cd $TMP/rc-scripts-${V} ; rm -rf `find -iname CVS` )
