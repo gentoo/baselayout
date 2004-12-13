f6f65b2eeb93ddc5233e9a445af859e9fe2eae7a
Martin Schlemmer <azarah@gentoo.org>
2004-12-13 19:19:06 +0000

    Tweak init.d/halt.sh to support new text based udev_db ('udevinfo -d' do not work anymore, and there is no space after '(S|N):').  Also check if devices.tar.bz2 is empty before trying to create it, bug #71143.
    svn path=/trunk/; revision=788

--- init.d/halt.sh
+++ init.d/halt.sh
@@ -25,9 +25,9 @@ elif [ ! -e /dev/.devfsd -a -e /dev/.udev \
 then
 	ebegin "Saving device nodes"
 	# Handle our temp files
-	devices_udev="`mktemp /tmp/devices.udev.XXXXXX`"
-	devices_real="`mktemp /tmp/devices.real.XXXXXX`"
-	device_tarball="`mktemp /tmp/devices-XXXXXX`"
+	devices_udev="$(mktemp /tmp/devices.udev.XXXXXX)"
+	devices_real="$(mktemp /tmp/devices.real.XXXXXX)"
+	device_tarball="$(mktemp /tmp/devices-XXXXXX)"
 	
 	if [ -z "${devices_udev}" -o -z "${devices_real}" -o \
 	     -z "${device_tarball}" ]
@@ -39,18 +39,39 @@ then
 		find . -xdev -type b -or -type c -or -type l | cut -d/ -f2- > \
 			"${devices_real}"
 		# Figure out what udev created
-		udevinfo -d | awk '/^N|S: ..*/ { i=1; while (i++<NF) { print $i}}' > \
-			"${devices_udev}"
+		eval $(grep '^[[:space:]]*udev_db=' /etc/udev/udev.conf)
+		if [ -d "${udev_db}" ]
+		then
+			# New udev_db is clear text ...
+			udevinfo="$(cat "${udev_db}"/*)"
+		else
+			# Old one is not ...
+			udevinfo="$(udevinfo -d)"
+		fi
+		# This basically strips 'S:' and 'N:' from the db output, and then
+		# print all the nodes/symlinks udev created ...
+		echo "${udevinfo}" | gawk '
+			/^(N|S):.+/ {
+				sub(/^(N|S):/, "")
+				split($0, nodes)
+				for (x in nodes)
+					print nodes[x]
+			}' > "${devices_udev}"
 		# These ones we also do not want in there
 		for x in MAKEDEV core fd initctl pts shm stderr stdin stdout
 		do
 			echo "${x}" >> "${devices_udev}"
 		done
-		# Now only tarball those not created by udev
-		try tar -jclpf "${device_tarball}" \
-		        `fgrep -x -v -f "${devices_udev}" < "${devices_real}"`
-		try mv -f "${device_tarball}" /lib/udev-state/devices.tar.bz2
-		try rm -f "${devices_udev}" "${devices_real}"
+		tarball_devices="$(fgrep -x -v -f "${devices_udev}" < "${devices_real}")"
+		# Now only tarball those not created by udev if we have any
+		if [ -n "${tarball_devices}" ]
+		then
+			try tar -jclpf "${device_tarball}" ${tarball_devices}
+			try mv -f "${device_tarball}" /lib/udev-state/devices.tar.bz2
+			try rm -f "${devices_udev}" "${devices_real}"
+		else
+			rm -f /lib/udev-state/devices.tar.bz2
+		fi
 		eend 0
 	fi
 fi
