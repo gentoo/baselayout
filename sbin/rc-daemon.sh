b597e3be4d8b35d5884ef92466da79231b5dcd49
Roy Marples <uberlord@gentoo.org>
2005-03-01 17:59:12 +0000

    rc-daemon.sh now provides a working wrapper for start-stop-daemon fixes bug #7198
    removed ps calls from net scripts
    
    svn path=/trunk/; revision=1004

--- sbin/rc-daemon.sh
+++ sbin/rc-daemon.sh
@@ -1,396 +1,197 @@
-# Copyright 1999-2004 Gentoo Foundation
+# Copyright 1999-2005 Gentoo Foundation 
 # Distributed under the terms of the GNU General Public License v2
 # $Header$
 
-# RC functions to work with daemons (very alpha for now, and not finished)
+# RC functions to work with daemons
+# Basically we're a fancy wrapper for start-stop-daemon
+# and should be called as such. This means that our init scripts
+# should work as is with zero modification :)
+
+# We rely on the /proc filesystem a lot here to save shelling out
+# to userspace tools. This makes us quite fast but less portable I guess.
 
 RC_GOT_DAEMON="yes"
 
-[ "${RC_GOT_FUNCTIONS}" != "yes" ] && source /sbin/functions.sh
+[[ ${RC_GOT_FUNCTIONS} != "yes" ]] && source /sbin/functions.sh
 
-# Stuff for getpids() and co
-declare -ax MASTER_PID=
-declare -ax PID_LIST=
-DAEMONS=
-PID_FILES=
 RC_RETRY_KILL="no"
 RC_RETRY_TIMEOUT=1
 RC_RETRY_COUNT=5
 RC_FAIL_ON_ZOMBIE="no"
+RC_KILL_CHILDREN="no"
 
 # Override default settings with user settings ...
-#[ -f /etc/conf.d/rc ] && source /etc/conf.d/rc
+[[ -f /etc/conf.d/rc ]] && source /etc/conf.d/rc
 
-getpidfile() {
-	local x=
-	local y=
-	local count=0
-	local count2=0
+# void setup_daemon_vars(void)
+#
+# Setup our vars based on the start-stop-daemon command
+setup_daemon_vars() {
+	local -a sargs=( "${args%% -- *}" )
+	exeargs="-- ${args##* -- }"
+
+	local i j=${#sargs[@]}
+	for (( i=0; i<j; i++ )); do
+		case ${sargs[i]} in
+			-S|--start)
+				stopping=false
+				unset sargs[i]
+				;;
+			-K|--stop)
+				stopping=true
+				unset sargs[i]
+				;;
+			-n|--name)
+				[[ ${i} -lt ${j} ]] && name=${sargs[i+1]}
+				;;
+			-x|--exec)
+				[[ ${i} -lt ${j} ]] && exe=${sargs[i+1]}
+				;;
+			-p|--pidfile)
+				[[ ${i} -lt ${j} ]] && pidfile=${sargs[i+1]}
+				;;
+			--pid=*)
+				pidfile=${sargs[i]##--pid=}
+				;;
+		esac
+	done
+
+	ssdargs="${sargs[@]}"
+}
+
+# bool try_kill_pid(int pid, int sig)
+#
+# Repeatedly kill the pid with the given signal until it dies
+# Returns 0 if successfuly otherwise 1
+try_kill_pid() {
+	local pid=$1 sig=${2:-TERM} i s
 	
-	if [ "$#" -ne 1 -o -z "${DAEMONS}" ]
-	then
-		return 1
-	else
-		for x in ${DAEMONS}
-		do
-			if [ "${x}" != "$1" ]
-			then
-				count=$((count + 1))
-				continue
-			fi
-			
-			if [ -n "${PID_FILES}" ]
-			then
-				count2=0
-				
-				for y in ${PID_FILES}
-				do
-					if [ "${count}" -eq "${count2}" -a -f ${y} ]
-					then
-						echo "${y}"
-						return 0
-					fi
-					
-					count2=$((count2 + 1))
-				done
-				for y in ${PID_FILES}
-				do
-					if [ "$(eval echo \${y/${x}/})" != "${y}" -a -f ${y} ]
-					then
-						echo "${y}"
-						return 0
-					fi
-				done
-			else
-				if [ -f /var/run/${x}.pid ]
-				then
-					echo "/var/run/${x}.pid"
-					return 0
-				elif [ -f /var/run/${myservice}/${x}.pid ]
-				then
-					echo "/var/run/${myservice}/${x}.pid"
-					return 0
-				fi
-			fi
-			
-			count=$((count + 1))
-		done
-	fi
+	# We split RC_RETRY_TIMEOUT into tenths of seconds
+	# So we return as fast as possible
+	(( s=${RC_RETRY_TIMEOUT}/10 ))
 	
+	for (( i=0; i<RC_RETRY_COUNT*10; i++ )); do
+		[[ ! -d /proc/${pid} ]] && return 0
+		kill -s ${sig} ${pid}
+		LC_ALL=C sleep ${s}
+	done
+
 	return 1
 }
 
+# bool kill_pid(int pid, int ppid)
 #
-# Simple funtion to return the pids of all the daemons in $DAEMONS
-# in the array $PID_LIST, with the master pids in $MASTER_PID.
-#
-getpids() {
-	local x=
-	local count=0
-	local pidfile=
+# Kills the given pid
+# If a parent pid (ppid) is supplied, the pid has to be a child of ppid
+# Returns 1 if we fail to kill the pid (if it's valid) otherwise 0
+kill_pid() {
+	local pid=$1 ppid=$2 i
+	local a b c d e f g
 
-	if [ -n "${DAEMONS}" ]
-	then
-		for x in ${DAEMONS}
-		do
-			MASTER_PID[${count}]=
-			PID_LIST[${count}]=
+	# Check that our pid really exists
+	[[ ! -d /proc/${pid} ]] && return 0
 
-			pidfile="$(getpidfile ${x})"
-			if [ -n "${pidfile}" ]
-			then
-				MASTER_PID[${count}]="$(< ${pidfile})"
-			fi
-			if [ -n "$(pidof ${x})" ]
-			then
-				PID_LIST[${count}]="$(pidof ${x})"
-			fi
-			
-			count=$((count + 1))
-		done
+	# If supplied a ppid (parent pid) then check that it still a child
+	# of the pid still. If not, return 0 as we haven't failed to kill
+	# as it's not a valid child
+	if [[ -n ${ppid} ]]; then
+		read a b c d e f g < /proc/${pid}/stat
+		[[ ${ppid} != ${e} ]] && return 0
 	fi
 
-	return 0
-}
+	# Now we kill the pid
+	try_kill_pid ${pid} TERM && return 0
 
-#
-# Return status:
-#   0 - Everything looks ok, or rc-script do not start any daemons
-#   1 - Master pid is dead, but other processes are running
-#   2 - Master pid and all others (if any), are dead
-#   3 - No pidfile, and no processes are running what so ever
-#
-checkpid() {
-	local x=
-	local count=0
-
-	if [ "$#" -ne 1 -o -z "$1" -o -z "${DAEMONS}" -o \
-	     "$(eval echo \${DAEMONS/$1/})" = "${DAEMONS}" ]
-	then
-		return 3
-	fi
-
-	getpids
-
-	for x in ${DAEMONS}
-	do
-		if [ "${x}" != "$1" ]
-		then
-			count=$((count + 1))
-			continue
-		fi
-
-		if [ -z "${PID_LIST[${count}]}" -a -n "${MASTER_PID[${count}]}" ]
-		then
-			return 2
-		elif [ -z "${PID_LIST[${count}]}" -a -z "${MASTER_PID[${count}]}" ]
-		then
-			return 3
-		elif [ -n "${MASTER_PID[${count}]}" -a ! -d /proc/${MASTER_PID[${count}]} ]
-		then
-			return 1
-		fi
-		
-		count=$((count + 1))
-	done
+	# Naughty pid - it didn't die nicely.
+	# Should we just KILL it?
+	[[ ${RC_RETRY_KILL} == "yes" ]] \
+		&& try_kill_pid ${pid} KILL && return 0
 	
-	return 0
+	# We failed to kill the pid :(
+	# But do a quick test to see if it's alive or not
+	[[ ! -d /proc/${pid} ]]
+	return $?
 }
 
-start-single-daemon() {
-	local retval=0
-	local pidfile=
-	local pidretval=0
-	local daemon=
-	local SSD="start-stop-daemon"
+# bool kill_children(int ppid)
+#
+# Stops all children of the given pid
+# Returns 1 if any fail to stop otherwise 0
+kill_children() {
+	local ppid=$1 pid retval=0
+	local children=$( pgrep -s ${ppid} )
 
-	if [ "$(eval echo \${DAEMONS/$1/})" != "${DAEMONS}" ]
-	then
-		daemon="$1"
-		shift
-
-		# Just a stupid sanity check, need to improve here.
-		# Basically we "check" that the daemon name is in
-		# the args to SSD.
-		if [ "$(eval echo \${*/${daemon}/})" = "$*" ]
-		then
-			return 1
-		fi
-	else
-		return 1
-	fi
-
-	if [ -z "${DAEMONS}" -o "$#" -lt 1 -o -z "${daemon}" ]
-	then
-		return 1
-	else
-		${SSD} $*
-		retval=$?
-
-		if [ "${retval}" -ne 0 ]
-		then
-			return ${retval}
-		fi
-
-		checkpid ${daemon}
-		pidretval=$?
-		# Need to rethink checkpid return values, as we prob
-		# need a switch or something that its ok, or not if
-		# the master pid is dead, but some instances are running.
-		if [ "${pidretval}" -ne 0 -a "${pidretval}" -ne 1 ]
-		then
-			return 1
-		fi
-	fi
+	for pid in "${children}"; do
+		kill_pid ${pid} ${ppid} || retval=1
+	done
 
 	return ${retval}
 }
 
+# int start_daemon(void)
 #
-# Stop a single daemon.  This is mainly used by stop-daemon().
-# It takes the following arguments:
-#
-#    --kill-pidfile    If the pidfile exists, remove it.
-#
-#    --fail-zombie     If the process was not running, exit with
-#                      a fail status.  Default is to exit cleanly.
-#
-stop-single-daemon() {
-	local retval=0
-	local pidfile=
-	local pidretval=0
-	local killpidfile="no"
-	local failonzombie="no"
-	local daemon=
-	local SSD="start-stop-daemon --stop --quiet"
+# We don't do anyting fancy - just pass the given options
+# to start-stop-daemon and return the value
+start_daemon() {
+	/sbin/start-stop-daemon ${args}
+	return $?
+}
 
-	for x in $*
-	do
-		case ${x} in
-			--kill-pidfile)
-				killpidfile="yes"
-				;;
-			--fail-zombie)
-				failonzombie="yes"
-				;;
-			*)
-				if [ "$(eval echo \${DAEMONS/${x}/})" != "${DAEMONS}" ]
-				then
-					if [ -n "${daemon}" ]
-					then
-						return 1
-					fi
-					
-					daemon="${x}"
-				fi
-				;;
-		esac
-	done
-
-	if [ -z "${DAEMONS}" -o "$#" -lt 1 -o -z "${daemon}" ]
-	then
-		return 1
+# bool stop_daemon(void)
+#
+# Instead of calling start-stop-daemon we instead try and
+# kill the process ourselves and any children left over
+# Returns 0 if everything was successful otherwise 1
+stop_daemon() {
+	local pids retval=0
+	
+	if [[ -s ${pidfile} ]]; then
+		pids=$( < ${pidfile} )
+	elif [[ -n ${exe} ]]; then
+		pids=$( pidof ${exe} )
+	elif [[ -n ${name} ]]; then
+		pids=$( pidof ${name} )
 	else
-		checkpid ${daemon}
-		pidretval=$?
-		if [ "${pidretval}" -eq 0 ]
-		then
-			pidfile="$(getpidfile ${daemon})"
-			if [ -n "${pidfile}" ]
-			then
-				${SSD} --pidfile ${pidfile}
-				retval=$?
-			else
-				${SSD} --name ${daemon}
-				retval=$?
-			fi
-		elif [ "${pidretval}" -eq 1 ]
-		then
-			${SSD} --name ${daemon}
-			retval=$?
-		elif [ "${pidretval}" -eq 2 ]
-		then
-			if [ "${RC_FAIL_ON_ZOMBIE}" = "yes" -o "${failonzombie}" = "yes" ]
-			then
-				retval=1
-			fi
-		elif [ "${pidretval}" -eq 3 ]
-		then
-			if [ "${RC_FAIL_ON_ZOMBIE}" = "yes" -o "${failonzombie}" = "yes" ]
-			then
-				retval=1
-			fi
-		fi
+		# We are given nothing to stop, so fail
+		return 1
 	fi
 
-	#only delete the pidfile if the daemon is dead
-	if [ "${killpidfile}" = "yes" ]
-	then
-		checkpid ${daemon}
-		pidretval=$?
-		if [ "${pidretval}" -eq 2 -o "${pidretval}" -eq 3 ]
-		then
-			rm -f $(getpidfile ${x})
+	for pid in "${pids}"; do
+		if [[ ${RC_FAIL_ON_ZOMBIE} == "yes" ]]; then
+			[[ ! -d /proc/${pid} ]] && return 1
 		fi
-	fi
 
-	#final sanity check
-	if [ "${retval}" -eq 0 ]
-	then
-		checkpid ${daemon}
-		pidretval=$?
-		if [ "${pidretval}" -eq 0 -o "${pidretval}" -eq 1 ]
-		then
-			retval=$((retval + 1))
+		if kill_pid ${pid} ; then
+			# Remove the pidfile if the process didn't
+			[[ -f ${pidfile} ]] && rm -f ${pidfile}
+		else
+			retval=1
 		fi
-	fi
+	
+		if [[ ${RC_KILL_CHILDREN} == "yes" ]]; then
+			kill_children ${pid} || retval=1
+		fi
+	done
 	
 	return ${retval}
 }
 
+# int start-stop-daemon(...)
 #
-# Should be used to stop daemons in rc-scripts.  It will
-# stop all the daemons in $DAEMONS.  The following arguments
-# are supported:
-#
-#    --kill-pidfile    Remove the pidfile if it exists (after
-#                      daemon is stopped)
-#
-#    --fail-zombie     If the process is not running, exit with
-#                      a fail status (default is to exit cleanly).
-#
-#    --retry           If not sucessfull, retry the number of times
-#                      as specified by $RC_RETRY_COUNT
-#
-stop-daemons() {
-	local x=
-	local count=0
-	local retval=0
-	local tmpretval=0
-	local pidretval=0
-	local retry="no"
-	local ssdargs=
+# Provide a wrapper to start-stop-daemon
+# Return the result of start_daemon or stop_daemon depending on
+# how we are called
+start-stop-daemon() {
+	local args=" $* " ssdargs exeargs
+	local exe name pidfile pid stopping
+	setup_daemon_vars
 
-	if [ -z "${DAEMONS}" ]
-	then
-		return 0
-	fi
-
-	for x in $*
-	do
-		case ${x} in
-			--kill-pidfile)
-				ssdargs="${ssdargs} --kill-pidfile"
-				;;
-			--fail-zombie)
-				ssdargs="${ssdargs} --fail-zombie"
-				;;
-			--retry)
-				retry="yes"
-				;;
-			*)
-				eerror "  ERROR: invalid argument to stop-daemon()!"
-				return 1
-				;;
-		esac
-	done
-
-	if [ "${retry}" = "yes" -o "${RC_RETRY_KILL}" = "yes" ]
-	then
-		for x in ${DAEMONS}
-		do
-			count=0
-			pidretval=0
-		
-			while ([ "${pidretval}" -eq 0 -o "${pidretval}" -eq 1 ]) && \
-				  [ "${count}" -lt "${RC_RETRY_COUNT}" ]
-			do
-				if [ "${count}" -ne 0 -a -n "${RC_RETRY_TIMEOUT}" ]
-				then
-					sleep ${RC_RETRY_TIMEOUT}
-				fi
-
-				stop-single-daemon ${ssdargs} ${x}
-				tmpretval=$?
-
-				checkpid ${x}
-				pidretval=$?
-
-				count=$((count + 1))
-			done
-			
-			retval=$((retval + tmpretval))
-		done
+	if ${stopping}; then
+		stop_daemon 
 	else
-		for x in ${DAEMONS}
-		do
-			stop-single-daemon ${ssdargs} ${x}
-			retval=$((retval + $?))
-		done
+		start_daemon
 	fi
-
-	return ${retval}
+	return $?
 }
-
 
 # vim:ts=4
