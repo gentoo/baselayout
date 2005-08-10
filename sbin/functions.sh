f5ad07c08be04d18ac1e2c49602df41ca8c99412
Mike Frysinger <vapier@gentoo.org>
2005-08-10 22:36:34 +0000

    add info for RC_BOOTLOG
    svn path=/branches/rc-scripts-1_6/; revision=1396

--- sbin/functions.sh
+++ sbin/functions.sh
@@ -1,6 +1,5 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
-# $Header$
 
 RC_GOT_FUNCTIONS="yes"
 
@@ -92,7 +91,7 @@ import_addon profiling-functions.sh
 bootlog() {
 	return 0
 }
-import_addon bootlogger.sh
+[[ ${RC_BOOTLOG} == "yes" ]] && import_addon bootlogger.sh
 
 # void get_bootconfig()
 #
