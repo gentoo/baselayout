e6dbdca3a26792ceca6bac9ad981137fb451c00d
Mike Frysinger <vapier@gentoo.org>
2005-11-20 12:44:16 +0000

    call reboot with -k to support kexec #80220
    svn path=/trunk/; revision=1619

--- init.d/reboot.sh
+++ init.d/reboot.sh
@@ -1,7 +1,7 @@
 # Copyright 1999-2005 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
-/sbin/reboot -idp
+/sbin/reboot -idpk
 
 # hmm, if the above failed, that's kind of odd ...
 # so let's force a reboot
