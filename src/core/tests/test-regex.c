2dc2da7b670d08f4d21031a4f4966862f6f57ea7
Martin Schlemmer <azarah@gentoo.org>
2005-12-03 00:30:19 +0000

    Syntax cleanups.  Make test-regex.c quiet.
    svn path=/trunk/; revision=1671

--- src/core/tests/test-regex.c
+++ src/core/tests/test-regex.c
@@ -22,7 +22,6 @@
  * $Header$
  */
 
-#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -31,50 +30,63 @@
 #include "librcscripts/simple-regex.h"
 
 char *test_data[] = {
-	/* string, pattern, match (1 = yes, 0 = no) */
-	"ab", "a?[ab]b", "1",
-	"abb", "a?[ab]b", "1",
-	"aab", "a?[ab]b", "1",
-	"a", "a?a?a?a", "1",
-	"aa", "a?a?a?a", "1",
-	"aa", "a?a?a?aa", "1",
-	"aaa", "a?a?a?aa", "1",
-	"ab", "[ab]*", "1",
-	"abc", "[ab]*.", "1",
-	"ab", "[ab]*b+", "1",
-	"ab", "a?[ab]*b+", "1",
-	"aaaaaaaaaaaaaaaaaaaaaaa", "a*b", "0",
-	"aaaaaaaaabaaabbaaaaaa", "a*b+a*b*ba+", "1",
-	"ababababab", "a.*", "1",
-	"baaaaaaaab", "a*", "0",
-	NULL
+  /* string, pattern, match (1 = yes, 0 = no) */
+  "ab", "a?[ab]b", "1",
+  "abb", "a?[ab]b", "1",
+  "aab", "a?[ab]b", "1",
+  "a", "a?a?a?a", "1",
+  "aa", "a?a?a?a", "1",
+  "aa", "a?a?a?aa", "1",
+  "aaa", "a?a?a?aa", "1",
+  "ab", "[ab]*", "1",
+  "abc", "[ab]*.", "1",
+  "ab", "[ab]*b+", "1",
+  "ab", "a?[ab]*b+", "1",
+  "aaaaaaaaaaaaaaaaaaaaaaa", "a*b", "0",
+  "aaaaaaaaabaaabbaaaaaa", "a*b+a*b*ba+", "1",
+  "ababababab", "a.*", "1",
+  "baaaaaaaab", "a*", "0",
+  NULL
 };
 
 int main() {
-	regex_data_t tmp_data;
-	char buf[256], string[100], regex[100];
-	int i;
+  regex_data_t tmp_data;
+  char buf[256], string[100], regex[100];
+  int i;
 
-	for (i = 0; NULL != test_data[i]; i += 3) {
-		snprintf(string, 99, "'%s'", test_data[i]);
-		snprintf(regex, 99, "'%s'", test_data[i + 1]);
-		snprintf(buf, 255, "string = %s, pattern = %s", string, regex);
-		printf("%-60s", buf);
-		DO_REGEX(tmp_data, test_data[i], test_data[i + 1], error);
-		if (REGEX_MATCH(tmp_data) && (REGEX_FULL_MATCH == tmp_data.match)) {
-			if (0 != strncmp(test_data[i + 2], "1", 1))
-				goto error;
-		} else {
-			if (0 != strncmp(test_data[i + 2], "0", 1))
-				goto error;
-		}
-			
-		printf("%s\n", "[ \033[32;01mOK\033[0m ]");
+  for (i = 0; NULL != test_data[i]; i += 3)
+    {
+      snprintf(string, 99, "'%s'", test_data[i]);
+      snprintf(regex, 99, "'%s'", test_data[i + 1]);
+      snprintf(buf, 255, "string = %s, pattern = %s", string, regex);
+#if TEST_VERBOSE
+      printf("%-60s", buf);
+#endif
+      
+      DO_REGEX(tmp_data, test_data[i], test_data[i + 1], error);
+      
+      if (REGEX_MATCH(tmp_data) && (REGEX_FULL_MATCH == tmp_data.match))
+	{
+	  if (0 != strncmp(test_data[i + 2], "1", 1))
+	    goto error;
+	}
+      else
+	{
+	  if (0 != strncmp(test_data[i + 2], "0", 1))
+	    goto error;
 	}
 
-	return 0;
-error:
-	printf("%s\n", "[ \033[31;01m!!\033[0m ]");
+#if TEST_VERBOSE
+      printf("%s\n", "[ \033[32;01mOK\033[0m ]");
+#endif
+    }
 
-	return 1;
+  return 0;
+  
+error:
+#if TEST_VERBOSE
+  printf("%s\n", "[ \033[31;01m!!\033[0m ]");
+#endif
+
+  return 1;
 }
