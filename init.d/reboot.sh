b08cc840864b22cae929082c49abb1979f2ad1f5
Diego Elio Pettenò <flameeyes@gentoo.org>
2006-02-26 13:12:04 +0000

    Make sure that halt and reboot are called with the right arguments. This adds a regression as now 'shutdown -r now' halts the system.
    svn path=/branches/bsd-porting-2/; revision=1932

--- init.d/reboot.sh
+++ init.d/reboot.sh
@@ -1,8 +1,12 @@
 # Copyright 1999-2006 Gentoo Foundation
 # Distributed under the terms of the GNU General Public License v2
 
-/sbin/reboot -idpk
+case $(uname -s) in
+	Linux | GNU* ) opts="-idk"; forceopts="-f" ;;
+esac
+
+/sbin/reboot -p "${opts}"
 
 # hmm, if the above failed, that's kind of odd ...
 # so let's force a reboot
-/sbin/reboot -f
+/sbin/reboot "${forceopts}"
