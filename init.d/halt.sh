033f5fb87472e665d83488a6dbf44021c9d9d9f3
Roy Marples <uberlord@gentoo.org>
2006-04-24 13:27:31 +0000

    Fixed everyones borked sort command in halt.sh
    svn path=/branches/bsd-porting-2/; revision=2016

--- init.d/halt.sh
+++ init.d/halt.sh
@@ -82,7 +82,7 @@ unmounts=$( \
 	        ($1 != "none") && \
 	        ($1 !~ /^(rootfs|\/dev\/root)$/) && \
 	        ($2 != "/")) \
-	      print $2 }' /proc/mounts | sort -k2 -ur)
+	      print $2 }' /proc/mounts | sort -ur)
 for x in ${unmounts}; do
 	# Do not umount these if we are booting off a livecd
 	if [[ -n ${CDBOOT} ]] && \
