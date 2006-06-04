712bfb46f7d4c6ed0bba845af8206aa2007eda55
Martin Schlemmer <azarah@gentoo.org>
2006-06-04 14:24:32 +0000

    Add missing tests/test-services.c.
    svn path=/trunk/; revision=2061

---
+++ src/core/tests/test-services.c
@@ -0,0 +1,51 @@
+/*
+ * test-services.c
+ *
+ * Test for services functionality.
+ *
+ * Copyright (C) 2004-2006 Martin Schlemmer <azarah@nosferatu.za.org>
+ *
+ *
+ *      This program is free software; you can redistribute it and/or modify it
+ *      under the terms of the GNU General Public License as published by the
+ *      Free Software Foundation version 2 of the License.
+ *
+ *      This program is distributed in the hope that it will be useful, but
+ *      WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *      General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License along
+ *      with this program; if not, write to the Free Software Foundation, Inc.,
+ *      675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Header$
+ */
+
+#include <stdlib.h>
+
+#include "rcscripts/rccore.h"
+
+int
+main (int argc, char **argv)
+{
+  /* Needed to init config variables, etc! */
+  rc_init ();
+
+  if (argc >= 2)
+    {
+      if (rc_service_test_state (argv[1], rc_service_started))
+	{
+	  EINFO ("Service '%s': started\n", argv[1]);
+	  exit (EXIT_SUCCESS);
+	}
+      else
+	{
+	  EERROR ("Service '%s': stopped\n", argv[1]);
+	  exit (EXIT_FAILURE);
+	}
+    }
+
+  return 0;
+}
+
