61b3027392a5f4b62151c7645234e034f09cae4a
Roy Marples <uberlord@gentoo.org>
2006-09-26 18:16:12 +0000

    Return after recalling - duh
    svn path=/trunk/; revision=2285

--- sbin/runscript.sh
+++ sbin/runscript.sh
@@ -188,6 +188,7 @@ svc_start_scheduled() {
 	if [[ ${IN_BACKGROUND} == "true" || ${IN_BACKGROUND} == "1" ]] ; then
 		unset IN_BACKGROUND
 		svc_start_scheduled &
+		return
 	fi
 
 	for x in $(dolisting "${svcdir}/scheduled/${SVCNAME}/") ; do
