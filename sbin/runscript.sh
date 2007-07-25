bae8e6593383b956e4e7c73ec416a92c9e8bf9c2
Roy Marples <uberlord@gentoo.org>
2007-07-25 21:22:42 +0000

    Patch runscript.sh to allow for --quiet status, #186459    thanks to Jan Kundrát.
    svn path=/branches/baselayout-1_12/; revision=2795

--- sbin/runscript.sh
+++ sbin/runscript.sh
@@ -8,7 +8,10 @@
 [[ ${RC_GOT_FUNCTIONS} != "yes" ]] && source /sbin/functions.sh
 
 # User must be root to run most script stuff (except status)
-if [[ ${EUID} != "0" ]] && ! [[ $2 == "status" && $# -eq 2 ]] ; then
+if [[ ${EUID} != "0" ]] && ! [[ $2 == "status" && $# -eq 2 ]] && \
+		! [[ $2 == "--quiet" && $3 == "status" && $# -eq 3 ]] && \
+		! [[ $3 == "--quiet" && $2 == "status" && $# -eq 3 ]] \
+		; then
 	eerror "$0: must be root to run init scripts"
 	exit 1
 fi
